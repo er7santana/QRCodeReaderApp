eb789647179652d46fc557ae2124b662dd81a332
Eliezer Sant Ana <eliezer.ana@santander.com.br>
2022-08-03 21:42:06 -0300

    Initial Commit

---
+++ QRCodeReaderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeReaderApp
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 03/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
