c4a4ae6fda8da628ffdaf5003ebdfbf4174f8cb0
Eliezer Sant Ana <eliezer.ana@santander.com.br>
2022-08-04 00:53:39 -0300

    Initial Commit

--- QRCodeReaderApp/ViewController.swift
+++ QRCodeReaderApp/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var qrCodeLabel: UILabel!
+    
+    private lazy var pickerController: UIImagePickerController = {
+       let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.allowsEditing = false
+        pickerController.mediaTypes = ["public.image"]
+        pickerController.sourceType = .photoLibrary
+        return pickerController
+    }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func didTapOnRead(_ sender: Any) {
+        navigationController?.present(pickerController, animated: true)
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let qrcodeImg = info[UIImagePickerController.InfoKey.originalImage] as? UIImage,
+           let detector: CIDetector = CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: [ CIDetectorAccuracy: CIDetectorAccuracyHigh]) {
+            
+            guard let ciImage: CIImage = CIImage(image: qrcodeImg) else {
+                qrCodeLabel.text = "Erro ao ler QR Code"
+                return
+            }
+            var qrCodeLink = ""
+            
+            guard let features = detector.features(in: ciImage) as? [CIQRCodeFeature] else {
+                qrCodeLabel.text = "Erro ao ler QR Code"
+                return
+            }
+            
+            for feature in features {
+                qrCodeLink += feature.messageString ?? ""
+            }
+            
+            if qrCodeLink == ""{
+                qrCodeLabel.text = "Nenhum QR Code encontrado"
+            } else {
+                qrCodeLabel.text = qrCodeLink
+            }
+        } else {
+            qrCodeLabel.text = "Erro ao ler QR Code"
+        }
+        self.dismiss(animated: true)
+    }
 
 }
 
